6633502c1a48f3a2e7946f30147d3f8fe80c7b03
Kabir Oberai <oberai.kabir@gmail.com>
2024-12-31 16:59:07 +0530

    Schema changes

--- Sources/PackCore/PackOutput.swift
+++ Sources/PackCore/PackOutput.swift
@@ -1,15 +1,63 @@
 import Foundation
 
 public struct PackOutput: Sendable, Codable {
-    public enum Version: Int, Codable, Sendable {
-        case v1 = 1
+    public enum Errors: Error {
+        case versionMismatch(expected: Int, actual: Int)
     }
 
-    public var version: Version
+    private static let version = 1
+
     public var path: String
 
     public init(path: String) {
-        self.version = .v1
         self.path = path
+    }
+}
+
+extension PackOutput {
+    private static let encoder: JSONEncoder = {
+        let encoder = JSONEncoder()
+        encoder.outputFormatting = [.sortedKeys, .withoutEscapingSlashes]
+        return encoder
+    }()
+
+    private static let decoder = JSONDecoder()
+
+    private struct Wrapper: Codable {
+        var version: Int
+        var output: PackOutput
+
+        enum CodingKeys: CodingKey {
+            case version
+        }
+
+        init(output: PackOutput) {
+            self.version = PackOutput.version
+            self.output = output
+        }
+
+        init(from decoder: any Decoder) throws {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+            self.version = try container.decode(Int.self, forKey: .version)
+            guard version == PackOutput.version else {
+                throw Errors.versionMismatch(expected: PackOutput.version, actual: version)
+            }
+            self.output = try decoder.singleValueContainer().decode(PackOutput.self)
+        }
+
+        func encode(to encoder: any Encoder) throws {
+            var container = encoder.container(keyedBy: CodingKeys.self)
+            try container.encode(self.version, forKey: .version)
+            var singleContainer = encoder.singleValueContainer()
+            try singleContainer.encode(self.output)
+        }
+    }
+
+    public static func decode(_ data: Data) throws -> PackOutput {
+        try Self.decoder.decode(Wrapper.self, from: data).output
+    }
+
+    public func encode() throws -> Data {
+        try Self.encoder.encode(Wrapper(output: self))
     }
 }
